729131c192ed8526cbcdf68b78928dc78abc8664
sekimura <sekimura@gmail.com>
2010-09-26 22:47:42 -0700

    use fullscreentube.com only for youtube and vimeo

--- app.rb
+++ app.rb
@@ -17,7 +17,10 @@ end
 get '/open/*' do
    u = params[:splat][0]
    uri = URI.parse(u)
-   uri.host = 'www.fullscreentube.com'
+   for h in ['youtube.com', 'vimeo.com' ]
+      uri.host = 'www.fullscreentube.com' if /#{h}$/.match(uri.host)
+   end
+   p uri.host
    cmd = ['/usr/bin/osascript', 'bin/open-browser.scpt', uri]
    system(cmd.join(" "))
 end
