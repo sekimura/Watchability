fa3ace0f01c26d354a9853d3739abb0d881cedb8
sekimura <sekimura@gmail.com>
2010-09-26 22:48:10 -0700

    remove debug print

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,9 +1,4 @@
-console.log(h);
-console.log(t);
-
 var u = 'http://' + h + '/open/' + escape(t);
-console.log(u);
-
 var f = document.createElement('iframe');
 f.src = u;
 document.body.appendChild(f);
